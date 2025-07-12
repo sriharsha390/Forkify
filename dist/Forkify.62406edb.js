c8ab8dfa174503714ca4346aaf301e97be1c3efd
harsha <sriharsha390@gmail.com>
2025-07-12 12:32:20 +0530

    commit4

--- dist/Forkify.62406edb.js
+++ dist/Forkify.62406edb.js
@@ -235,18 +235,20 @@ ${recipe.ingredients.map((el)=>{
               </div>
             </li>`;
     }).join('')}
+           
+          </ul>
         </div>
 
         <div class="recipe__directions">
           <h2 class="heading--2">How to cook it</h2>
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+            <span class="recipe__publisher">The Pioneer Woman</span>. Please check out
             directions at their website.
           </p>
           <a
             class="btn--small recipe__btn"
-            href="${recipe.source_url}"
+            href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
             target="_blank"
           >
             <span>Directions</span>
